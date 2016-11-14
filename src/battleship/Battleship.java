4e29c01b1507edb6e4290d215cc8cc60b373fae6
Justin Rivera <jrivera2@umw.edu>
2016-11-14 01:57:00 -0500

    Initial set up

---
+++ src/battleship/Battleship.java
@@ -0,0 +1,8 @@
+package battleship;
+
+//import java.util.ArrayList;
+public class Battleship {
+    public static void main(String[] args) {
+    //java.util.Scanner input = new java.util.Scanner(System.in);
+    }
+}
---
+++ src/battleship/players/jrivera2_BattleshipPlayer.java
@@ -0,0 +1,43 @@
+
+package battleship.players;
+
+import battleship.Board;
+
+public class jrivera2_BattleshipPlayer implements BattleshipPlayer {
+
+    /**
+     * hideShips - This method is called once at the beginning of each game
+     * when you need to hide your ships.
+     *
+     * You must return a valid Board object. See that class for details.
+     * Note carefully: under *no* circumstances should you return the same
+     * board twice in a row; i.e., two successive calls to your hideShips()
+     * method must always return *different* answers!
+     */
+    public Board hideShips() {
+        // INSERT AMAZING CODE HERE
+        return null;
+    }
+
+    /**
+     * go - This method is called repeatedly throughout the game, every
+     * time it's your turn.
+     *
+     * When it's your turn, and go() is called, you must call fireAt() on
+     * the Board object which is passed as a parameter. You must do this
+     * exactly *once*: trying to fire more than once during your turn will
+     * be detected as cheating.
+     */
+    public void go(Board opponentsBoard) {
+        // INSERT INCREDIBLE CODE HERE
+    }
+
+    /**
+     * reset - This method is called when a game has ended and a new game
+     * is beginning. It gives you a chance to reset any instance variables
+     * you may have created, so that your BattleshipPlayer starts fresh.
+     */
+    public void reset() {
+        // RESET YOUR OBJECT HERE
+    }
+}
