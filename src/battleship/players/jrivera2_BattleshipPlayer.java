787fcbb39973c394beee4118c7b84885b86b6916
Justin Rivera <jrivera2@umw.edu>
2016-11-20 22:06:17 -0500

    Set up var

--- src/battleship/players/jrivera2_BattleshipPlayer.java
+++ src/battleship/players/jrivera2_BattleshipPlayer.java
@@ -1,43 +1,136 @@
-
 package battleship.players;
 
 import battleship.Board;
+import java.util.Random;
 
 public class jrivera2_BattleshipPlayer implements BattleshipPlayer {
 
+    int[][] pastShot = {
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
+    int[][] oldBoards = {
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
+    int[][] currentBoard = {
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
+    int[][] lastBoard = {
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
+    int[][] lastestBoard = {
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
+    char[][] myBoard = new char[10][10];
+    Random rng = new Random();
+
     /**
-     * hideShips - This method is called once at the beginning of each game
-     * when you need to hide your ships.
+     * hideShips - This method is called once at the beginning of each game when
+     * you need to hide your ships.
      *
-     * You must return a valid Board object. See that class for details.
-     * Note carefully: under *no* circumstances should you return the same
-     * board twice in a row; i.e., two successive calls to your hideShips()
-     * method must always return *different* answers!
+     * You must return a valid Board object. See that class for details. Note
+     * carefully: under *no* circumstances should you return the same board
+     * twice in a row; i.e., two successive calls to your hideShips() method
+     * must always return *different* answers!
      */
     public Board hideShips() {
-        // INSERT AMAZING CODE HERE
+        fill(myBoard);
+        copy(lastBoard, lastestBoard);
+        copy(currentBoard, lastBoard);
+        fill(currentBoard);
+
         return null;
     }
 
     /**
-     * go - This method is called repeatedly throughout the game, every
-     * time it's your turn.
+     * go - This method is called repeatedly throughout the game, every time
+     * it's your turn.
      *
-     * When it's your turn, and go() is called, you must call fireAt() on
-     * the Board object which is passed as a parameter. You must do this
-     * exactly *once*: trying to fire more than once during your turn will
-     * be detected as cheating.
+     * When it's your turn, and go() is called, you must call fireAt() on the
+     * Board object which is passed as a parameter. You must do this exactly
+     * *once*: trying to fire more than once during your turn will be detected
+     * as cheating.
      */
     public void go(Board opponentsBoard) {
-        // INSERT INCREDIBLE CODE HERE
     }
 
     /**
-     * reset - This method is called when a game has ended and a new game
-     * is beginning. It gives you a chance to reset any instance variables
-     * you may have created, so that your BattleshipPlayer starts fresh.
+     * reset - This method is called when a game has ended and a new game is
+     * beginning. It gives you a chance to reset any instance variables you may
+     * have created, so that your BattleshipPlayer starts fresh.
      */
     public void reset() {
         // RESET YOUR OBJECT HERE
+    }
+
+    private void fill(int[][] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                arr[i][j] = 0;
+            }
+        }
+    }
+
+    private void fill(char[][] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                arr[i][j] = ' ';
+            }
+        }
+    }
+
+    private void copy(char[][] arr1, char[][] arr2) {
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < arr1.length; j++) {
+                arr2[i][j] = arr1[i][j];
+            }
+        }
+    }
+
+    private void copy(int[][] arr1, int[][] arr2) {
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < arr1.length; j++) {
+                arr2[i][j] = arr1[i][j];
+            }
+        }
     }
 }
